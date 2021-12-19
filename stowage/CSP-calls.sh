8ee0ccacee6c46c34b750e17a568ad60525df861
mestre99 <48585228+mestresas99@users.noreply.github.com>
2021-12-19 01:32:27 +0100

    Delete stowage directory

--- stowage/CSP-calls.sh
+++
@@ -1,6 +0,0 @@
-python3 CSPStowage.py ./CSP-tests/ map.txt containers.txt
-python3 CSPStowage.py ./CSP-tests/ map1.txt containers1.txt
-python3 CSPStowage.py ./CSP-tests/ map2.txt containers2.txt
-python3 CSPStowage.py ./CSP-tests/ map3.txt containers3.txt
-python3 CSPStowage.py ./CSP-tests/ map4.txt containers4.txt
-python3 CSPStowage.py ./CSP-tests/ map5.txt containers5.txt
