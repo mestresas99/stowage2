170a5d3834c1d5b27a8147ad6a306a00352100ac
xion16lm <100383016@alumnos.uc3m.es>
2021-12-19 12:04:17 +0100

    Comment code

--- stowage/a-star.py
+++ stowage/a-star.py
@@ -6,7 +6,7 @@ import numpy as np
 
 class Node():
     def __init__(self, position=None):
-        self.containers = {}
+        self.containers = {} #list of all containers and their positions
         self.map = []
         self.fCost = 0
         self.position = position #postion can be 0,1,2 depends on the port we are
@@ -20,19 +20,19 @@ class Node():
     def __eq__(self, other):
         return self.position == other.position
 
-global position_all_containers
+global position_all_containers #global variable for the position of all the containers
 position_all_containers={}
-def readInputs():
+def readInputs(): #read files for each of the inputs
     if len(sys.argv)!=5:
         return len(sys.argv),len(sys.argv)
     path = sys.argv[1]
     map_file = sys.argv[2]
     containers_file = sys.argv[3]
-    global heur
+    global heur #variable of the type of heuristic
     heur = sys.argv[4]
     path_map = path + map_file
     path_containers = path + containers_file
-    global boat_map
+    global boat_map #variable that includes the map of the boat
     boat_map = []
     containers =[]
     with open(path_map, 'r') as fmap:
@@ -44,7 +44,8 @@ def readInputs():
             boat_map.append(i.split(" "))
             counter += 1
         fmap.close()
-    global next_cell_available
+    global next_cell_available #cells that mus be occupied next taking into account gravity
+    global next_cell_available_original #first cell that is available i each of the stacks of the boat (auxiliary variable for recalculating next_cell_available)
     next_cell_available = np.zeros(len(boat_map[0]))
     for i in range(len(boat_map[0])):
         obtained = False
@@ -68,15 +69,16 @@ def readInputs():
     boat_map_first_len = len(boat_map)
     global boat_map_cols
     boat_map_cols = len(boat_map[0])
+    next_cell_available_original = next_cell_available.copy()
     return boat_map,containers
 
 
-def load(container, position):
-    position_dict = position_all_containers.deepcopy()
-    position_dict.update({container[0]:position})
+def load(container, position): #creates the children state that will come from applying a load operation to the parent
+    position_dict = position_all_containers.copy()
+    position_dict.update({container[0]:position}) #changes the position of a container
     return position_dict
 
-def isPossible(container,position):
+def isPossible(container,position): #sees if a position is correct (refrigerated and non refrigerated containers)
     if container[1] =="R":
         if boat_map[int(position[0])][position[1]] == "E":
             return True
@@ -90,39 +92,39 @@ def isPossible(container,position):
         else:
             return False
 
-def hasArrived(container):
+def hasArrived(container): #sees it the container is already in the destination port
     if container[0] in arrived:
         return True
     else:
         return False
 
 arrived = []
-def unload(container,port):
-    position_dict = position_all_containers.deepcopy()
+def unload(container,port): #creates the child states that come from an unloading operation from the parent
+    position_dict = position_all_containers.copy()
     position = position_dict[container[0]]
     pos = -1 - position
-    position_dict[container[0]] = [pos,pos]
+    position_dict[container[0]] = [pos,pos] #-1 for por 0, -2 for port 1 and -3 for port 2
     if container[2] == port:
-        arrived.append(container[0])
+        arrived.append(container[0])  #check if the port of unloading is the destination
     return position_dict
 
-def sail(node, port):
+def sail(node, port): #changes the port of a node (not used in the final implementation)
     node.position = port
 
-def isLoaded(container):
+def isLoaded(container): #checks if a container is already in he boat or not
     if position_all_containers[container[0]][0]<0:
         return False
     else:
         return True
 
-def samePort(container,position_boat):
+def samePort(container,position_boat): #check if we are in the same port that an unloaded container, or if we have sailed to another location
     position = position_all_containers[container[0]]
     if position[0] == (-1 - position_boat):
         return True
     else:
         return False
 
-def heuristic1(node,containers):
+def heuristic1(node,containers): #checks the number of operations left for arriving to the goal (relaxing the restriction of loading contianers that are on different port than when we are and supposing that we must arrive to port 2 at the end)
     num_to_load = 0
     unloads = 0
     for c in position_all_containers.keys():
@@ -140,7 +142,7 @@ def heuristic1(node,containers):
 
 #def heuristic2():
 
-def bubble_sort(open):
+def bubble_sort(open): #function for sorting the vector of nodes
     n = len(open)
 
     for i in range(n-1):
@@ -149,7 +151,7 @@ def bubble_sort(open):
                 open[j], open[j+1] = open[j+1], open[j]
     return open
 
-def generateChildren(current_node,containers):
+def generateChildren(current_node,containers): #calls the load and unload methods and create all the children for a given parent
     children = []
     for container in containers:
         if (not isLoaded(container)) and (not hasArrived(container)) and (samePort(container,current_node.position)):
@@ -176,8 +178,8 @@ def generateChildren(current_node,containers):
             
     return children
 
-def recalculate_next_cell_available(containers):
-    min_pos =1000*np.ones(boat_map_cols)
+def recalculate_next_cell_available(containers): #recalculate the positions where we can add the containers
+    min_pos = next_cell_available_original.copy()
     for i in containers.keys():
         if containers[i][0]>=0:
             if containers[i][0] - 1 < min_pos[containers[i][1]]:
@@ -185,11 +187,10 @@ def recalculate_next_cell_available(containers):
                 print(containers[i][0] -1)
                 min_pos[containers[i][1]] = containers[i][0] - 1
     for j in range(len(min_pos)):
-        if min_pos[j]!=1000:
-            next_cell_available[j] = min_pos[j]
+        next_cell_available[j] = min_pos[j]
     print(min_pos)
         
-def astar(start, containers, map):
+def astar(start, containers, map): #a-star implementation
     start_node = Node(start)
     start_node.map = map
     open = []
@@ -202,7 +203,8 @@ def astar(start, containers, map):
     while len(open)>0 and not exit:
         current_node = open.pop(0)
         close.append(current_node)
-        position_all_containers = current_node.containers.deepcopy()
+        global position_all_containers
+        position_all_containers = current_node.containers.copy()
         recalculate_next_cell_available(current_node.containers)
         print("next cell av loop" + str(next_cell_available))
         #check if it is goal
@@ -238,13 +240,19 @@ def astar(start, containers, map):
         #         node.g = 3500 * np.abs(i-current_node.position)
         #         childrens.append(node)
         
-        for child in children:
+        for child in children: #see if the children are in the closed vector, in the open vector and change the cost if they are already in the open vector
             found = False
-            for closed_node in close:
+             # Create the f and h values
+            if heur == "heuristic1": #calculate the heuristic
+                child.h = heuristic1(child,containers) #call to the heuristic
+            else:
+                pass #space for the second heuristic
+            child.f = child.g + child.h
+            for closed_node in close: #check if it is in closed_node
                 if child.containers == closed_node.containers and child.position == closed_node.position:
                     found=True
             if found == False:
-                for open_node in open:
+                for open_node in open: #check if it is in the open node
                     if child.containers == open_node.containers and child.position == open_node.position:
                         found = True
                         if child.f < open_node.f:
@@ -252,19 +260,12 @@ def astar(start, containers, map):
                             open.append(child)
             if found == False:
                 open.append(child) 
-            
-            # Create the f, g, and h values
-            if heur == "heuristic1":
-                child.h = heuristic1(child,containers) #call to the heuristic
-            else:
-                pass
-            child.f = child.g + child.h
-        open = bubble_sort(open)
-        if len(open)>0:
+        open = bubble_sort(open) #sort the open vector from less cost to more
+        if len(open)>0: #just debugging
             print(open[1].containers)
             
 
-    if exit:
+    if exit: #close should be change with path (to be fixed)
         solution = close
     else:
         solution = None
